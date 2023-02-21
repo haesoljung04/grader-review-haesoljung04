bb9b83bc2ff4a958e5cabc2a3adfcbb5b17b3435
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2023-02-21 12:03:11 -0800

    lab skills week 6

--- grade.sh
+++ grade.sh
@@ -1,5 +1,22 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
+set +e
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
+if [[ -f ListExamples.java ]]
+then
+    echo "ListExamples.java found"
+else
+    echo "ListExamples.java not found"
+    exit 1
+fi
+cp ../TestListExamples.java ./
+cp -r ../lib ./
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2>javac-errors.txt
+if [[ $? -ne 0 ]]
+then
+    echo "Compilation failed"
+    exit 1
+fi
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples >testResults.txt
